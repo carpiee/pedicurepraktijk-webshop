af93ec1fffdebe0e500fb95e7cae769c3955636d
carpiee <62463826+carpiee@users.noreply.github.com>
2020-08-30 13:47:38 +0200

    last update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,34 @@
 module.exports = {
-  prefix: "",
-  important: false,
-  separator: ":",
-  theme: {
-    colors: {},
-    // ...
-    zIndex: {},
+  purge: {
+    enabled: true,
+    content: [
+      "./templates/*.php",
+      "./product/*.php",
+      "./js/*.js",
+      "./inc/*.php",
+      "./inc/*.html",
+      "./cart/*.php",
+      "./cart/templates/*.php",
+      "./admin/*.php",
+      "./admin/templates/*.php",
+      "./admin/cms/*.php",
+      "./account/*.php",
+      "./account/templates/*.php",
+      "./account/registreer/*.php",
+      "./account/login/*.php",
+      "./*.php",
+    ],
   },
+  theme: {},
   variants: {
     appearance: ["responsive"],
-    backgroundColor: ["responsive", "hover", "focus", "active"],
+    backgroundColor: ["responsive", "hover", "focus", "active", "group-hover"],
     opacity: ["responsive", "hover", "focus", "disabled"],
     zIndex: ["responsive", "hover", "focus"],
-    borderWidth: ["responsive", "first", "hover", "focus"],
+    borderWidth: ["responsive", "first", "hover", "focus", "group-hover"],
     width: ["responsive", "first", "hover", "focus"],
     space: ["responsive", "hover", "focus"],
+    textColor: ["responsive", "hover", "focus", "active", "group-hover"],
   },
-  plugins: [
-    // ...
-  ],
+  plugins: [],
 };
